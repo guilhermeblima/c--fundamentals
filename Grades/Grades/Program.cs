607fe3f8a0b626f4a298cafdaf5f381cfb04a1dd
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-12-20 16:31:09 +1300

    delegates and events

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -12,16 +12,21 @@ namespace Grades
         static void Main(string[] args)
         {
 
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-            synth.Speak("Yo, sup bro, Alan here, I'm gonna compute the grades!!");
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+            //synth.Speak("Yo, sup bro, Alan here, I'm gonna compute the grades!!");
 
             GradeBook book = new GradeBook();
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Gui's grade book";
             book.AddGrade(3f);
             book.AddGrade(8f);
             book.AddGrade(9f);
             book.AddGrade(3f);
+
             GradeStatistics stats = book.ComputeStatistics();
 
+            Console.WriteLine(book.Name);
             WriteResults("Average",stats.AverageGrade);
             WriteResults("Highest",stats.HighestGrade);
             WriteResults("Lowest",stats.LowestGrade);
@@ -31,6 +36,11 @@ namespace Grades
         static void WriteResults(string description, float grade)
         {
             Console.WriteLine(description+": " + grade);
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"The name has been updated from {args.ExsistingName} to {args.NewName}");
         }
     }
 }
